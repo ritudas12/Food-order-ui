a23b317ae741937df2c2275008410b199ff00907
Ritu Das <dasritu056@gmail.com>
2025-07-14 11:25:51 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+        <img src={assets.logo2} alt="" />
+        <p>Made with ❤️ and lots of good food!  
+Serving happiness one meal at a time.  
+© 2025 QuickCrave | Let's eat together 🍔🍕🥗
+</p>
+        <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+        </div>
+        </div>
+        <div className="footer-content-center">
+            <h2>COMPANY</h2>
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Delivery</li>
+                <li>Privacy Policy</li>
+            </ul>
+        </div>
+        <div className="footer-content-right">
+            <h2>GET IN TOUCH</h2>
+            <ul>
+                <li>+1-212-345-9876</li>
+                <li>contact@QuickCrave.com</li>
+            </ul>
+        </div>
+       
+      </div>
+      <hr />
+     
+      <p className="footer-copyright">
+        copyright 2024 @ QuickCrave.com - All Right Reserved. 
+      </p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Skip the Line — Order Online!</h2>
+        <p>Enjoy a wide variety of cuisines, seamless online ordering, and fast, reliable delivery. Discover your next culinary adventure with us! </p>
+        <button>View menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
